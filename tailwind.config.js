eec600bda082ff30e77a3dac63db82c102e0921d
Julien Delort <julien.delort@gmail.com>
2021-04-19 10:31:09 -0400

    Fixed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   purge: [
     './public/**/*.html',
     './pages/**/*.{js,jsx,ts,tsx,vue}',
+    './components/**/*.{js,jsx,ts,tsx,vue}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
